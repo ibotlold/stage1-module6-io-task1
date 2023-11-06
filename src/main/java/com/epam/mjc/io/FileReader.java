882f2529883f1c60e0d11cef5ac4d52b6c05443b
Kirill <ibotlold@gmail.com>
2023-11-06 23:13:32 +0600

    feat: solution

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        HashMap<String, String> map = new HashMap();
+        try (FileInputStream fileStream = new FileInputStream(file)) {
+            int ch;
+            StringBuilder builder = new StringBuilder();
+            String key = "";
+            String value = "";
+            while ((ch = fileStream.read()) != -1) {
+                builder.delete(0, builder.length());
+                builder.append((char) ch);
+                while ((ch = fileStream.read()) != -1) {
+                    if ((char) ch == ':') {
+                        break;
+                    }
+                    builder.append((char) ch);
+                }
+                key = builder.toString();
+                builder.delete(0, builder.length());
+                fileStream.skip(1);
+                while ((ch = fileStream.read()) != -1) {
+                    if ((char) ch == '\n') {
+                        break;
+                    }
+                    builder.append((char) ch);
+                }
+                value = builder.toString();
+                map.put(key, value);
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return new Profile(map.get("Name"), Integer.parseInt(map.get("Age")), map.get("Email"), Long.parseLong(map.get("Phone")));
     }
 }
